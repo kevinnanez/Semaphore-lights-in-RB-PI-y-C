129f9c66aae24aca9ea7062e3b2c955f54c47408
Kevin Braian Ñañez <kevin93nz@gmail.com>
2016-10-18 23:39:40 -0300

    Archivos de catedra

---
+++ rbpi/main.c
@@ -0,0 +1,23 @@
+
+int main void{
+//Declarar variables
+	unsigned int i = 0;
+	unsigned int N = 10;
+	uint8_t tiempo;
+	uint8_t estado;
+
+	if(tiempo = 0.000){
+		for (i = 0; i < N; ++i){
+			//recorro el array modo_0[10]
+		}
+	} else{
+		//aumento la secuencia
+	}
+
+	while(estado == 00001001 || estado == 00010001 || estado == 00001010 || estado == 00000000) {
+		//el ultimo es ilogico, deberia ser programado en base al tiempo q esten apagadas
+		//modo emergencia: loop array modo_1[1]
+		//hasta cuando hace el loop?
+	}
+
+}
